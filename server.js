fe780d698320aff4ec795935cf5795b3c0e6d8b4
saidugurlu <ugurlumsaid@gmx.de>
2022-06-20 11:52:23 +0200

    dynamic menu works

--- server.js
+++ server.js
@@ -12,27 +12,36 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./src/views"));
 
-app.get("/", (req, res) => {
-  res.render("pages/index", siteData);
+app.get('/', (req, res) => {
+  siteData.currentPage = 'welcome';
+  res.render('pages/index', siteData);
 });
 
-app.get("/nouns", (req, res) => {
-  res.render("pages/nouns", siteData);
+app.get('/nouns', (req, res) => {
+  siteData.currentPage = 'nouns';
+  res.render('pages/nouns', siteData);
 });
 
 app.get('/books', (req, res) => {
-    res.render('pages/books', siteData)
+  siteData.currentPage = 'books';
+  res.render('pages/books', siteData);
 });
+
 app.get('/translations', (req, res) => {
-  res.render('pages/translations', siteData)
+  siteData.currentPage = 'translations';
+  res.render('pages/translations', siteData);
 });
+
 app.get('/landscape-photos', (req, res) => {
-  res.render('pages/landscape-photos', siteData)
+  siteData.currentPage = 'landscape-photos';
+  res.render('pages/landscape-photos', siteData);
 });
 
 app.get('/jobs', (req, res) => {
-  res.render('pages/jobs', siteData)
+  siteData.currentPage = 'jobs';
+  res.render('pages/jobs', siteData);
 });
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
